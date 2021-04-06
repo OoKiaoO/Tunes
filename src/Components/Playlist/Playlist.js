f737315a3113a46ccba33601335a1610a0391a4d
Chiara Campalani <campalanichiara@gmail.com>
2021-04-06 16:39:21 +0900

    made Playlist name editable

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -3,10 +3,21 @@ import TrackList from '../TrackList/TrackList';
 import './Playlist.css';
 
 class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(e) {
+    const newName = e.target.value;
+    this.props.onNameChange(newName);
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input defaultValue={'New Playlist'} />
+        <input onChange={this.handleNameChange} defaultValue={'New Playlist'} />
           <TrackList isRemoval={true} onRemove={this.props.onRemove} tracks={this.props.playlistTracks} />
         <button className="Playlist-save">SAVE TO SPOTIFY</button>
       </div>
